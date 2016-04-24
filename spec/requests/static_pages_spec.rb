d7f9681217d5728b9912f566901e6c9cfeef4f01
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-04-24 21:21:12 +0300

    Add naed routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,17 @@ describe "Static pages" do
 
 	describe "Home page" do
 		it "should have the content 'Sample App'" do
-				visit '/static_pages/home'
-				expect(page).to have_content('Sample App')
+			visit root_path
+			expect(page).to have_content('Sample App')
 		end
 
 		it "should have right title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).to have_title("#{base_title}")
 		end
 
 		it "should have right title" do
-			visit '/static_pages/home'
+			visit root_path
 			expect(page).not_to have_title("| Home")
 		end
 	end
@@ -24,12 +24,12 @@ describe "Static pages" do
 	describe "Help page" do
 
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')
 		end
 
 		it "should have right title" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_title("#{base_title} | Help")
 		end
 	end
@@ -37,12 +37,12 @@ describe "Static pages" do
 	describe "About page" do
 
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 
 		it "should have right title" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_title("#{base_title} | About Us")
 		end
 	end
@@ -50,12 +50,12 @@ describe "Static pages" do
 	describe "Contact page" do
 
 		it "should have the content 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_content('Contact')
 		end
 
 		it "should have the title 'Contact'" do
-			visit '/static_pages/contact'
+			visit contact_path
 			expect(page).to have_title("#{base_title} | Contact")
 		end	
 	end
