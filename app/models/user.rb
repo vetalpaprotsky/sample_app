31319a16dede306fd05c2e0e235357fac8cdbfcc
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-04-26 23:20:47 +0300

    Add validations to model User

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase } # => стовбець email має індекс..через це нам треба перевести
+  # => сам емеїл в нижній регістир тому що не всі адаптери БД використовують регістрозалежні індекси
+	validates :name,  presence: true, length: { maximum: 50 }
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+					  uniqueness: { case_sensitive: false }
+end
