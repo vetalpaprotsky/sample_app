df22c68ef11ca66420f5ebeec2ae946593c473cf
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-04-29 16:44:17 +0300

    Add more secure email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
-	before_save { self.email = email.downcase } # => стовбець email має індекс..через це нам треба перевести
+	#before_save { self.email = self.email.downcase } # => те саме що і нижня стрічка
+	before_save { email.downcase! } # => стовбець email має індекс..через це нам треба перевести
   # => сам емеїл в нижній регістир тому що не всі адаптери БД використовують регістрозалежні індекси
 	validates :name,  presence: true, length: { maximum: 50 }
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 					  uniqueness: { case_sensitive: false }
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,7 @@ describe User do
 	describe "when email format is invalid" do
 		it "should be invalid" do
 			addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
       	@user.email = invalid_address
       	expect(@user).not_to be_valid
@@ -72,12 +72,12 @@ describe User do
 
 	describe "when password doesn't match confirmation" do
 		before { @user.password_confirmation = "mismatch" }
-		it { should_not be_valid }
+		it { should be_invalid }
 	end
 
 	describe "with a password that's too short" do
 		before { @user.password = @user.password_confirmation = "a" * 5 }
-		it { should_not be_valid }
+		it { should be_invalid }
 	end
 
 	describe "return value of authenticate method" do
@@ -93,6 +93,16 @@ describe User do
 
 			it { should_not eq user_for_invalid_password }  
 			specify { expect(user_for_invalid_password).to be_false } # => 'specify' the save as 'it' 
+		end
+	end
+
+	describe "email address with mixed-case" do
+		let(:mixed_case_email) { "Foo@EXampLe.cOm" }
+
+		it "should be saved as all lover-case" do
+			@user.email = mixed_case_email
+			@user.save
+			expect(@user.reload.email).to eq mixed_case_email.downcase
 		end
 	end
 
