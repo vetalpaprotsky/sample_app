5b873382a702750d5d131441627a3460182b3fa6
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-05-01 13:43:58 +0300

    Add rspec tests for form

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,6 +32,17 @@ describe "User pages" do
 				#final = User.count
 				#expect(initial).to eq final
 			end
+			describe "after submission" do
+				before { click_button submit }
+
+				it { should have_title "Sign up" }
+				it { should have_content "error" }
+				it { should have_content "Name can't be blank" }
+				it { should have_content "Email can't be blank" }
+				it { should have_content "Email is invalid" }
+				it { should have_content "Password is too short (minimum is 6 characters)" }
+				it { should have_content "Password can't be blank" }
+			end
 		end
 
     describe "with valid information" do
@@ -44,6 +55,15 @@ describe "User pages" do
 
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
+      end
+
+      describe "after submission" do
+      	before { click_button submit }
+      	let(:user) { User.find_by(email: 'user@example.com') }
+
+      	it { should have_title(user.name) }
+      	it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      	it { should have_selector('h1', text: "Example User") }
       end
 		end
 	end
