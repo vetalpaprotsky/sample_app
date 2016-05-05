d203dd62b203f6f55d87ad5d228c3ce9ac51892a
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-05-05 20:08:25 +0300

    Add users' updating

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,17 +12,17 @@ class User < ActiveRecord::Base
 	validates :password, length: { minimum: 6 }
 	has_secure_password
 
-	def User.new_remember_token
-		SecureRandom.urlsafe_base64
-	end
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
 
-	def User.encrypt(token)
-		Digest::SHA1.hexdigest(token.to_s)
-	end
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
 
-	private
+  private
 
-		def create_remember_token
-			self.remember_token = User.encrypt(User.new_remember_token)
-		end
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
 end
