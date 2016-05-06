740d48825917826bd849ddb354d2831a5d01dfb6
vetalpaprotsky <vetalpaprotsky@gmail.com>
2016-05-06 11:46:47 +0300

    finish users' authorization

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -44,4 +44,55 @@ describe "Authentication" do
     end
   end
 
+  describe "authentication" do
+
+    describe "for non-signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+
+      describe "when attempting to visit a protected page" do
+        before do
+          visit edit_user_path(user)
+          fill_in "Email",  with: user.email
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        describe "after signing in" do
+          it "should render the desired protected page" do
+            expect(page).to have_title("Edit user")
+          end
+        end 
+      end
+
+      describe "in the Users controller" do
+        describe "visit the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) } # => patch - відправляє HTTP запрос PATCH по адресу user_path(user)
+          specify { expect(response).to redirect_to(signin_path) } # => є так само get, post i delete
+        end # => response - це є низькорівневий об’єкт який преставляє відповіть сервера
+      end
+    end
+
+    describe "as wrong user" do
+      let(:user) { FactoryGirl.create(:user) }
+      let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
+      before { sign_in user, no_capybara: true }
+
+      describe "submitting a GET request to the Users#edit action" do
+        before { get edit_user_path(wrong_user) }
+        specify { expect(response.body).not_to match(full_title('Edit user')) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+
+      describe "submitting a PATCH request ro the Users#update action" do
+        before { patch user_path(wrong_user) }
+        specify { expect(response).to redirect_to(root_url) }
+      end
+    end
+  end
+
 end
